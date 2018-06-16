e2c9b51b38a24ed492b9ffc5378f4ddefd7a829f
Young Jin Park <youngjinpark20@gmail.com>
2018-06-16 01:09:11 +0000

    add easy development fixes

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "rules": {
+    "no-underscore-dangle": 0,
+    "quote-props": 0,
+    "no-param-reassign": 0,
+  },
+};
